c96e9d631e2ab75ad136e8b9616e6a11b7aacefc
Shazeb Suhail <shazebs7@outlook.com>
2024-03-17 21:22:43 -0700

    router blob photos fix

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from './pages/HomePage.vue'; 
 import Login from './pages/LoginPage.vue';
 import Signup from './pages/SignupPage.vue';
-import Photos from './components/BlobPhotos.vue'; 
+// import Photos from './components/BlobPhotos.vue'; 
 import About from './pages/AboutPage.vue';
 import Account from './pages/AccountPage.vue';
 import Receipt from './components/ReceiptPage.vue';
@@ -13,7 +13,7 @@ const routes = [
     { path: '/:username', component: Home },
     { path: '/login', component: Login },
     { path: '/signup', component: Signup },
-    { path: '/photos', component: Photos },
+    // { path: '/photos', component: Photos },
     { path: '/about', component: About },
     { path: '/account', component: Account },
     { path: '/receipt', component: Receipt }

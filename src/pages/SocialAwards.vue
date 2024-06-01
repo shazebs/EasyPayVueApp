f424d795351862309dc0dea578dc7da9fe41ff31
Shazeb Suhail <shazebs7@outlook.com>
2024-05-31 22:27:43 -0700

    error popup

--- src/pages/SocialAwards.vue
+++ src/pages/SocialAwards.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h1 align="center">Welcome to Social Awards by @shazebs</h1>
+
+        <div class="error-message" v-if="errors.disconnect" :class="{'slide-in': errors.disconnect}">
+            <span></span>You are disconnected!<br/>Try refreshing the page.<span @click="closeError('disconnect')">&times;</span>
+        </div>
+
         <button @click="sendMessage">Send Message</button>
     </div>
 </template>
@@ -10,18 +15,31 @@ export default {
     name: 'SocialAwards',
     data() {
         return {
-            connection: null
+            connection: null,
+            errors: {
+                disconnect: false
+            }
         }
     },
     methods: {
-        sendMessage() {
+        async sendMessage() {
             const data = {
                 message: 'Hello World!',
                 client_id: 999
             };
             const data_string = JSON.stringify(data);
-            this.connection.send(data_string);
-        }
+
+            if (this.connection.readyState === WebSocket.OPEN) 
+            {
+                this.connection.send(data_string);
+            } 
+            else {
+                this.errors.disconnect = true;
+            }   
+        },
+        closeError(errorKey) {
+            this.errors[errorKey] = false;
+        },
     },
     created() {
         console.log("Starting Connection to WebSocket Server");
@@ -42,16 +60,47 @@ export default {
 
         this.connection.onclose = (event) => {
             console.log(event);
-            alert('You are disconnected, try refreshing the page.');
         }
 
         this.connection.onerror = (error) => {
-            console.log(error);
+            console.log('[websocket error]', error);
+            this.errors.disconnect = true;
         }
     }
 }
 </script>
 
 <style scoped>
+    @keyframes slideIn {
+        from {
+            transform:translateX(-100%);
+        }
+        to {
+            transform: translateX(0);
+        }
+    }
+    .slide-in {
+        animation: slideIn 0.5s forwards;
+    }
 
+    .error-message {
+        align-items: center;
+        background: lightcoral; 
+        border: 2px solid red; 
+        border-radius: 8px;
+        display: flex;
+        justify-content: space-between;
+        margin: 10px auto; 
+        min-width: 300px; 
+        padding: 3px; 
+        width: 50%;
+    }
+        .error-message span {
+            font-size:larger;
+            margin-right:5px; 
+        }
+            .error-message span:hover {
+                color:white; 
+                cursor:pointer;
+            }
 </style>
